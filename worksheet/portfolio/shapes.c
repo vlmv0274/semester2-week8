e985011f5701b4b3208594199f31cc0ef1fa1a58
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-23 03:34:14 +0000

    Update shapes.c

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -31,3 +31,33 @@ Triangle makeTriangle(Point p1, Point p2, Point p3) {
     return new;
 }
 
+//Structure calculation functions
+
+float lineLength(Line l) {
+    float dx = l.p[1].x - l.p[0].x;
+    float dy = l.p[1].y - l.p[0].y;
+    //Euclidean distance
+    return sqrt((dx * dx) + (dy * dy));
+}
+
+//herons had issues, shoelace attempt
+float triangleArea(Triangle t) {
+    float x1 = t.p[0].x, y1 = t.p[0].y;
+    float x2 = t.p[1].x, y2 = t.p[1].y;
+    float x3 = t.p[2].x, y3 = t.p[2].y;
+    float area = x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2);
+    return fabs(area) / 2.0;
+}
+
+//Structure comparison functions
+bool samePoint(Point p1, Point p2) {
+    return fabs(p1.x - p2.x) < 1.0e-6 && fabs(p1.y - p2.y) < 1.0e-6;
+}
+
+bool pointInLine(Point p, Line l) {
+    return samePoint(p, l.p[0]) || samePoint(p, l.p[1]);
+}
+
+bool pointInTriangle(Point p, Triangle t) {
+    return samePoint(p, t.p[0]) || samePoint(p, t.p[1]) || samePoint(p, t.p[2]);
+}
