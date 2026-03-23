a2cdc0224bc0d47ae231b84c6dc6aac3c8d97a71
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-23 03:02:42 +0000

    Update shapes.c

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -7,6 +7,8 @@
 Point makePoint( float x, float y ) {
     Point new;
     // implementation
+    new.x = x;
+    new.y = y;
     return new;
 }
 
@@ -14,3 +16,18 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+Line makeLine(Point p1, Point p2) {
+    Line new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    return new;
+}
+
+Triangle makeTriangle(Point p1, Point p2, Point p3) {
+    Triangle new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    new.p[2] = p3;
+    return new;
+}
+
